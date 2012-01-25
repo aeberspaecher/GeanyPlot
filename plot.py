71a4e932a3539021dc7c89d60ce5face9a9f4a21
Alexander Eberspächer <alex.eberspaecher@gmail.com>
2012-01-25 17:03:17 +0100

    Add GPL license notice.

--- plot.py
+++ plot.py
@@ -1,11 +1,29 @@
 #!/usr/bin/env python
 #-*- coding:utf-8 -*-
 
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+
+# Copyright 2012 Alexander Eberspächer <alex.eberspaecher@googlemail.com>
+
 """A Geany plugin for data visualization written in Python.
 
 The plugin is supposed to be used for quick data inspection without the need
 to run a Python session just for the purpose of visualization. The plugin uses
-Matplotlib.
+Matplotlib and the Geany's Python bindings available from
+https://github.com/codebrainz/geanypy
 """
 
 import gtk
