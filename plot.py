d8dca735b0fcb4541df3f9a010000bc546ff0aa2
Alexander Eberspächer <alex.eberspaecher@gmail.com>
2012-01-25 15:18:26 +0100

    Fixed crash when program exits.

--- plot.py
+++ plot.py
@@ -11,7 +11,13 @@ Matplotlib.
 import gtk
 import geany
 import numpy as np
-import matplotlib.pyplot as plt
+
+import matplotlib
+matplotlib.use('GTKAgg')
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_gtkagg import FigureCanvasGTKAgg as FigureCanvas
+from matplotlib.backends.backend_gtkagg import NavigationToolbar2GTKAgg as NavigationToolbar
+
 
 class GeanyPlot(geany.Plugin):
 
@@ -28,10 +34,31 @@ class GeanyPlot(geany.Plugin):
 
     def cleanup(self):
         self.menu_item.destroy()
+        # remove canvas? figure? anything else?
 
     def on_plot_item_clicked(widget, data):
         # load data TODO: support more than two columns and more than one dataset
         fileName = geany.document.get_current().file_name
         x, y = np.loadtxt(fileName, usecols=(0,1), unpack=True)
-        plt.plot(x, y)
-        plt.show()
+
+        # create a new window
+        win = gtk.Window(gtk.WINDOW_TOPLEVEL)
+        win.connect("destroy", lambda x: gtk.main_quit())
+        win.set_default_size(600,450)
+        win.set_title("GeanyPlot")
+
+        vbox = gtk.VBox()
+        win.add(vbox)
+
+        fig = Figure(figsize=(5,4), dpi=100)
+        ax = fig.add_subplot(111)
+        ax.plot(x, y)
+
+        canvas = FigureCanvas(fig)  # a gtk.DrawingArea
+        vbox.pack_start(canvas)
+        toolbar = NavigationToolbar(canvas, win)
+        vbox.pack_start(toolbar, False, False)
+        
+        win.show_all()
+        gtk.main()
+
